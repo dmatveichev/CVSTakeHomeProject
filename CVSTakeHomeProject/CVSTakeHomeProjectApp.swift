fc7b08a7e04d6bf9df77d34cb0f0a76d49e37ddf
Dmitrii Matveichev <dmatveichev@icloud.com>
2024-11-26 14:26:35 -0500

    Initial Commit

---
+++ CVSTakeHomeProject/CVSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSTakeHomeProjectApp.swift
+//  CVSTakeHomeProject
+//
+//  Created by Dmitry Matveichev on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
