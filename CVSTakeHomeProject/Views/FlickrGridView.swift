dfd6b4133d836d47aea0d8bd562d6d5a87edceda
Dmitrii Matveichev <dmatveichev@icloud.com>
2024-11-27 09:16:16 -0500

    present error message in MainView

--- CVSTakeHomeProject/Views/FlickrGridView.swift
+++ CVSTakeHomeProject/Views/FlickrGridView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct FlickrGridView: View {
     @StateObject private var viewModel = FlickrPhotoViewModel()
     @Environment(\.imageCache) private var cache
+    
+    @Namespace private var namespace
+    @State var selectedPhoto: FlickrPhoto?
 
     var body: some View {
         NavigationView {
@@ -23,32 +26,41 @@ struct FlickrGridView: View {
                         }
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                     } else {
-                        ScrollView {
-                            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 12) {
-                                if viewModel.photos.isEmpty {
-                                    VStack {
-                                        Text("No results found")
-                                            .foregroundColor(.gray)
-                                            .multilineTextAlignment(.center)
-                                    }
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        if viewModel.photos.isEmpty {
+                            VStack {
+                                if let errorMessage = viewModel.errorMessage {
+                                    Text(errorMessage)
+                                        .foregroundColor(.red)
+                                        .multilineTextAlignment(.center)
+                                        .padding()
                                 } else {
+                                    Text("No results found")
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.center)
+                                        .padding()
+                                }
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        } else {
+                            ScrollView {
+                                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 12) {
                                     ForEach(viewModel.photos) { photo in
-                                        NavigationLink(destination: ImageDetailView(photo: photo)) {
-                                            VStack {
-                                                if let url = URL(string: photo.media.m) {
-                                                    CachedAsyncImage(url: url, cache: cache)
-                                                        .frame(width: 100, height: 100)
-                                                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                                                }
-                                                
-                                                Text(photo.title)
-                                                    .font(.caption)
-                                                    .lineLimit(1)
-                                                    .foregroundColor(.black)
+                                        VStack {
+                                            if let url = URL(string: photo.media.m) {
+                                                CachedAsyncImage(url: url, cache: cache)
+                                                    .frame(width: 100, height: 100)
+                                                    .clipShape(RoundedRectangle(cornerRadius: 8))
                                             }
+                                            
+                                            Text(photo.title)
+                                                .font(.caption)
+                                                .lineLimit(1)
+                                                .foregroundColor(.black)
                                         }
-                                        .buttonStyle(PlainButtonStyle())
+                                        .onTapGesture {
+                                            selectedPhoto = photo
+                                        }
+                                        .matchedTransitionSource(id: photo.id, in: namespace)
                                     }
                                 }
                             }
@@ -60,6 +72,10 @@ struct FlickrGridView: View {
             .navigationTitle("Flickr Photos")
             .onAppear {
                 viewModel.fetchPhotos()
+            }
+            .sheet(item: $selectedPhoto) { photo in
+                ImageDetailView(photo: photo)
+                    .navigationTransition(.zoom(sourceID: photo.id, in: namespace))
             }
         }
     }
